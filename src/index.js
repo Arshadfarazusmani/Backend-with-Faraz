001f2575287dfaa91e02820faf454e56956bd732
Arshad Faraz <arshadfarazusmani@gmail.com>
2025-01-08 12:47:16 +0530

    add server code to code base

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import{ app }from "./app.js"
 
 dotenv.config({
     path:"./env"
 })
+const port= process.env.PORT||1000;
 
 connectDB()
+.then(()=>{
+    app.listen(port , ()=>{
+        console.log("server is running at port :", port);
+        
+    })
+})
+.catch((err)=> {
+    console.log(`MongoDB connection Faild !!! ${err}`);
+    
+
+})
